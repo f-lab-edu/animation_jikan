fd751af964a478c7708dc421534d0ca602528a26
hyunwoo <beewoo01@naver.com>
2025-10-05 19:01:25 +0900

    chore : SuppressLint 제거

--- app/src/main/java/com/artem/animationjikan/ui/tab/search/search.kt
+++ app/src/main/java/com/artem/animationjikan/ui/tab/search/search.kt
@@ -1,11 +1,9 @@
 package com.artem.animationjikan.ui.tab.search
 
-import android.annotation.SuppressLint
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.artem.animationjikan.ui.theme.AnimationJikanTheme
 
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun SearchTab() {
 
