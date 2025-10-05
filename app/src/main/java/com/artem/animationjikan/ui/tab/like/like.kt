cf5b67945707c2459a0adac992b7481095c4d542
hyunwoo <beewoo01@naver.com>
2025-10-05 18:32:23 +0900

    chore: 불필요한 import문 제거

--- app/src/main/java/com/artem/animationjikan/ui/tab/like/like.kt
+++ app/src/main/java/com/artem/animationjikan/ui/tab/like/like.kt
@@ -1,6 +1,5 @@
 package com.artem.animationjikan.ui.tab.like
 
-import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
