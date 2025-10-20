e5e824975051dcb4955ddf7cd933f1b1110e3a0c
hyunwoo <beewoo01@naver.com>
2025-10-20 22:51:56 +0900

    chore: 불필요한 코드 제거

--- app/src/main/java/com/artem/animationjikan/presentation/ui/tab/home/home.kt
+++ app/src/main/java/com/artem/animationjikan/presentation/ui/tab/home/home.kt
@@ -25,7 +25,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.artem.animationjikan.R
 import com.artem.animationjikan.presentation.ui.tab.home.components.ContentSectionRow
 import com.artem.animationjikan.presentation.ui.tab.home.components.RecommendPager
@@ -44,8 +43,6 @@ fun HomeTab(
     LaunchedEffect(Unit) {
         viewModel.execute()
     }
-
-    val chipItems = CATEGORIES_LIST
 
     val scrollState = rememberScrollState()
 
