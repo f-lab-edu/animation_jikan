8dcde496b229bb672f8ad31774f3a4411dbf2ac5
hyunwoo <beewoo01@naver.com>
2025-11-14 09:41:33 +0900

    feature: character tab 수정하는 중

--- app/src/main/java/com/artem/animationjikan/presentation/ui/screen/detail/animation/tabs/character/CharacterTab.kt
+++ app/src/main/java/com/artem/animationjikan/presentation/ui/screen/detail/animation/tabs/character/CharacterTab.kt
@@ -6,9 +6,11 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -17,14 +19,17 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -47,7 +52,7 @@ fun CharacterTab() {
             color = colorResource(R.color.white)
         )
         Spacer(modifier = Modifier.height(10.dp))
-        Row {
+        Row(modifier = Modifier.height(IntrinsicSize.Min)) {
             AsyncImage(
                 model = ImageRequest.Builder(LocalContext.current)
                     .data("https://cdn.myanimelist.net/images/characters/13/519083.jpg?s=b280b410b588ebcd3fd30ac6fad02978")
@@ -67,44 +72,69 @@ fun CharacterTab() {
 
             Spacer(modifier = Modifier.width(10.dp))
 
-            Column {
-                Text(
-                    "성우",
-                    fontSize = 15.sp,
-                    lineHeight = 17.sp,
-                    maxLines = 1,
-                    fontWeight = FontWeight(400),
-                    color = colorResource(R.color.white)
-                )
-
-                Spacer(modifier = Modifier.height(10.dp))
-
-                FlowRow(
-                    horizontalArrangement = Arrangement.spacedBy(8.dp),
-                    verticalArrangement = Arrangement.spacedBy(8.dp),
-                    modifier = Modifier.fillMaxWidth()
+            Column(
+                modifier = Modifier.fillMaxHeight(),
+                verticalArrangement = Arrangement.SpaceBetween
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
                 ) {
-                    repeat(10) {
-                        Row {
-                            Box(
-                                modifier = Modifier
-                                    .size(45.dp)
-                            ) {
-                                AsyncImage(
-                                    model = ImageRequest.Builder(LocalContext.current)
-                                        .data("https://cdn.myanimelist.net/images/voiceactors/3/86543.jpg?s=e3313f29eea2aca127e39c9fc74c84e7")
-                                        .crossfade(true)
-                                        .build(),
-                                    contentDescription = null,
-                                    modifier = Modifier
-                                        .fillMaxSize()
-                                        .clip(CircleShape)
-                                        .clickable {
+                    Icon(
+                        modifier = Modifier.size(13.dp),
+                        painter = painterResource(R.drawable.ic_favorite_red_on),
+                        tint = Color.Unspecified,
+                        contentDescription = null
+                    )
+                    Spacer(modifier = Modifier.width(3.dp))
+                    Text(
+                        "4730",
+                        fontSize = 13.sp,
+                        lineHeight = 17.sp,
+                        maxLines = 1,
+                        fontWeight = FontWeight(400),
+                        color = colorResource(R.color.white)
+                    )
+                }
 
-                                        }
-                                        .background(color = Color.LightGray),
-                                    contentScale = ContentScale.Crop,
-                                )
+                Column {
+                    Text(
+                        "성우",
+                        fontSize = 15.sp,
+                        lineHeight = 17.sp,
+                        maxLines = 1,
+                        fontWeight = FontWeight(400),
+                        color = colorResource(R.color.white)
+                    )
+
+                    Spacer(modifier = Modifier.height(10.dp))
+
+                    FlowRow(
+                        horizontalArrangement = Arrangement.spacedBy(8.dp),
+                        verticalArrangement = Arrangement.spacedBy(8.dp),
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        repeat(10) {
+                            Row {
+                                Box(
+                                    modifier = Modifier
+                                        .size(45.dp)
+                                ) {
+                                    AsyncImage(
+                                        model = ImageRequest.Builder(LocalContext.current)
+                                            .data("https://cdn.myanimelist.net/images/voiceactors/3/86543.jpg?s=e3313f29eea2aca127e39c9fc74c84e7")
+                                            .crossfade(true)
+                                            .build(),
+                                        contentDescription = null,
+                                        modifier = Modifier
+                                            .fillMaxSize()
+                                            .clip(CircleShape)
+                                            .clickable {
+
+                                            }
+                                            .background(color = Color.LightGray),
+                                        contentScale = ContentScale.Crop,
+                                    )
+                                }
                             }
                         }
                     }
