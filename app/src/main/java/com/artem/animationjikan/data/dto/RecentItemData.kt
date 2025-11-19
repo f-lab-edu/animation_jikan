b1ab20820badacc9004989d1bb623c0d7d1abc04
hyunwoo <beewoo01@naver.com>
2025-11-20 08:35:20 +0900

    chore: string res 변수 추가 및 수정

---
+++ app/src/main/java/com/artem/animationjikan/data/dto/RecentItemData.kt
@@ -0,0 +1,4 @@
+package com.artem.animationjikan.data.dto
+
+class RecentItemData {
+}
---
+++ app/src/main/java/com/artem/animationjikan/presentation/ui/components/ComponentUtil.kt
@@ -0,0 +1,46 @@
+package com.artem.animationjikan.presentation.ui.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.artem.animationjikan.R
+
+@Composable
+fun ErrorWidget(messageStringResId: Int, contentDescription: Int? = null) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 80.dp, horizontal = 18.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.ic_warning),
+            contentDescription = contentDescription?.let { stringResource(id = contentDescription) },
+            tint = Color.Unspecified
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            stringResource(messageStringResId),
+            color = colorResource(R.color.grey4),
+            fontSize = 18.sp,
+            fontWeight = FontWeight.SemiBold,
+        )
+    }
+}
